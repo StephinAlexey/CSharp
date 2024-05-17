6c0fd5323e2a62c6f0b21334c5bb9146c95a1e78
Alexey <alexeystephin@gmail.com>
2024-05-17 20:42:39 +0300

    Lesson2:Task2 filling the array by foreach

---
+++ Курс знакомство с языками программирования/Lesson2/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int N;
+Console.WriteLine("Please write the length of array");
+int.TryParse(Console.ReadLine(),out N);
+int[] arr = new int[N];
+int i = 0;
+foreach (int item in arr)
+{
+    i++;
+    arr[i - 1] = i;
+    Console.Write($"{arr[i - 1]} ");
+}
+
+Console.WriteLine($"\n{arr.Length}");
