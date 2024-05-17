690fcc04981008b1b4bbd346ee9f300649cb3d42
Alexey <alexeystephin@gmail.com>
2024-05-17 12:25:06 +0300

    Lesson2:Task1 arrays

---
+++ Курс знакомство с языками программирования/Lesson2/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 6;
+arr[2] = 2;
+arr[3] = 3;
+arr[4] = 56;
+
+int[] arr2 = new int[5] { 5, 6, 2, 3, 56 };
+
+int[] arr3 = { 5, 6, 2, 3, 56 };
+
+foreach (int item in arr)
+    Console.WriteLine(item);
+
+foreach (int item in arr2)
+    Console.WriteLine(item);
+
+foreach (int item in arr3)
+    Console.WriteLine(item);
+
