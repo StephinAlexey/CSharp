fff0c176062481f319897068f10589e816e10b20
Alexey <alexeystephin@gmail.com>
2024-05-17 21:56:34 +0300

    Find max element in array by for and foreach

---
+++ Курс знакомство с языками программирования/Lesson2/Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿//Find max element in array
+int Arrlength = 0;
+
+Console.WriteLine("Please insert the array length");
+int.TryParse(Console.ReadLine(), out Arrlength);
+int[] arr = new int[Arrlength];
+Console.WriteLine("Please insert array's elements");
+for (int i = 0; i < Arrlength; i++)
+{
+    int.TryParse(Console.ReadLine(), out arr[i]);
+}
+int MaxElem = arr[0];
+foreach (int item in arr)
+{
+    if (item > MaxElem)
+        MaxElem = item;
+}
+Console.WriteLine($"The max element is {MaxElem}");
