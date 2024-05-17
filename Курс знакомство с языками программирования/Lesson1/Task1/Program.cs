c5802e407786d36292a2a44b9aea718c844de605
Alexey <alexeystephin@gmail.com>
2024-05-17 08:26:52 +0300

    Modified whith while&Swith

--- Курс знакомство с языками программирования/Lesson1/Task1/Program.cs
+++ Курс знакомство с языками программирования/Lesson1/Task1/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+bool flag=true;
+Console.Clear();
+while(flag)
+{
+  Console.WriteLine("Please insert 1 or 2 for correct printing\nInsert x for exit\n");
+  switch (Console.ReadLine())
+  {
+    case "1":
+    Console.Clear();
+    Console.WriteLine("Your input is 1");
+    break;
+    case "2":
+    Console.Clear();
+    Console.WriteLine("Your input is 2");
+    break;
+    case "x":
+    flag=false;
+    break;
+    default:
+    Console.Clear();
+    Console.WriteLine("Your input isn't 1 or 2");
+    break;
+
+  }
+}
