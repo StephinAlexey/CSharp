75f25a40d75a5ae1d7dead37c0bcd01fd25516ef
Alexey Stefin <alexeystephin@gmail.com>
2024-05-18 14:46:08 +0300

    Lesson3:Task3 ZeroEvenElementsOfArr

--- Курс знакомство с языками программирования/Lesson3/Task2/Program.cs
+++ Курс знакомство с языками программирования/Lesson3/Task2/Program.cs
@@ -5,8 +5,8 @@ void PrintSquares(int limit)
     Console.WriteLine($"Squares of numbers until {limit}: ");
     for (int i = 1; i <= limit; i++)
     {
-        Console.WriteLine($"{i} * {i} = {i*i}");
+        Console.WriteLine($"{i} * {i} = {i * i}");
     }
-} 
+}
 
 PrintSquares(12);
---
+++ Курс знакомство с языками программирования/Lesson3/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+void ZeroEvenElementsOfArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i += 2)
+    {
+        arr[i] = 0;
+    }
+}
+int[] arr = { 23, 12, 34, 56, 7, 45, 34, 121, 5 };
+ZeroEvenElementsOfArr(arr);
+foreach (int item in arr)
+{
+    Console.WriteLine(item);
+}
