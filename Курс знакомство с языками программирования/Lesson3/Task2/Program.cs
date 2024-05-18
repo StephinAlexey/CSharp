cd2a032492922b372eda224aeb5b001fe046e098
Alexey Stefin <alexeystephin@gmail.com>
2024-05-18 12:56:23 +0300

    Lesson3:Task2 PrintSquares function

---
+++ Курс знакомство с языками программирования/Lesson3/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+void PrintSquares(int limit)
+{
+    Console.WriteLine($"Squares of numbers until {limit}: ");
+    for (int i = 1; i <= limit; i++)
+    {
+        Console.WriteLine($"{i} * {i} = {i*i}");
+    }
+} 
+
+PrintSquares(12);
