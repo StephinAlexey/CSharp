e1f83cde61495bfbe2960d6bbadc694abae61412
Alexey Stefin <alexeystephin@gmail.com>
2024-05-18 11:56:20 +0300

    Lesson3:Task1 Calculate by function

---
+++ Курс знакомство с языками программирования/Lesson3/Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿//Calculate by function
+
+Console.WriteLine ($"The result is: {CalculateFormula(12.2,23.5,24,18)}");
+double CalculateFormula (double a, double b, double c, double d)
+{
+    return (a*b/(c+d)); 
+}
