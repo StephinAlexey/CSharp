5296dfcdfce04b6bc8b28383d2893be50ba05871
Alexey Stefin <alexeystephin@gmail.com>
2024-05-19 21:12:13 +0300

    Lesson5:Task1 Recursia, factorial

---
+++ Курс знакомство с языками программирования/Lesson5/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+int Factorial(int num)
+{
+    int k=num;
+    if (num == 0)
+    {
+        Console.WriteLine($"Раскрутка рекурсии num = {num}");
+        return 1;
+    }
+    num = Factorial(num - 1) * num;
+    Console.WriteLine($"Раскрутка рекурсии Factorial = {num} parameter = {k}");
+    return num;
+}
+int result = Factorial(5);
+Console.WriteLine(result);
