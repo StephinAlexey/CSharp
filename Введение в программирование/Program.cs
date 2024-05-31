5bae60f1dfafd548b8ecb59048916aa7cb8a3a53
StephinAlexey <alexeystephin@gmail.com>
2024-05-31 12:47:49 +0300

    Array's elements AVG

--- Введение в программирование/Program.cs
+++ Введение в программирование/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int[] numbers = { 2, 5, 13, 7, 6, 4 };
+int size = 6; //numbers.Length;
+double sum = 0;
+double avg = 0;
+for (int index = 0; index < size; index++)
+{
+    sum += numbers[index];
+};
+avg = sum / size;
+Console.WriteLine($"The array's elements avg= {avg}");
